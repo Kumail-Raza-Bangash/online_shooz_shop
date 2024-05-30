2b22cf4442c9e6263ad57f6ad3941cb7b7b4f15b
Kumail-Raza-Bangash <kumailraza260@gmail.com>
2024-05-30 14:43:57 +0500

    Updated Code

--- lib/view/main_screen.dart
+++ lib/view/main_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:online_shooz_shop/provider/main_screen_provider.dart';
 import 'package:online_shooz_shop/resources/colors.dart';
-import 'package:online_shooz_shop/resources/dimensions.dart';
 import 'package:online_shooz_shop/view/pages/add_page.dart';
 import 'package:online_shooz_shop/view/pages/cart_page.dart';
 import 'package:online_shooz_shop/view/pages/home_page.dart';
@@ -14,7 +13,7 @@ import 'package:provider/provider.dart';
 class MainScreen extends StatelessWidget {
   MainScreen({super.key});
 
-  List<Widget> pageList = [
+  List<Widget> pageList = const [
     HomePage(),
     SearchPage(),
     AddPage(),
@@ -29,51 +28,7 @@ class MainScreen extends StatelessWidget {
         return Scaffold(
           backgroundColor: AppColor.backgroundColor,
           body: pageList[myProvider.pageIndex],
-          bottomNavigationBar: SafeArea(
-            child: Container(
-              padding: EdgeInsets.all(Dimensions.width10),
-              margin: EdgeInsets.all(Dimensions.width10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(Dimensions.radius10),
-                color: AppColor.blackColor,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CustomBottomNavbar(
-                    onTap: () {
-                      myProvider.pageIndex = 0;
-                    },
-                    icon: Icons.home,
-                  ),
-                  CustomBottomNavbar(
-                    onTap: () {
-                      myProvider.pageIndex = 1;
-                    },
-                    icon: Icons.search,
-                  ),
-                  CustomBottomNavbar(
-                    onTap: () {
-                      myProvider.pageIndex = 2;
-                    },
-                    icon: Icons.add,
-                  ),
-                  CustomBottomNavbar(
-                    onTap: () {
-                      myProvider.pageIndex = 3;
-                    },
-                    icon: Icons.shopping_cart,
-                  ),
-                  CustomBottomNavbar(
-                    onTap: () {
-                      myProvider.pageIndex = 4;
-                    },
-                    icon: Icons.person,
-                  ),
-                ],
-              ),
-            ),
-          ),
+          bottomNavigationBar: const BottomNavBar(),
         );
       },
     );
--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:online_shooz_shop/resources/app_style.dart';
-import 'package:online_shooz_shop/resources/colors.dart';
 import 'package:online_shooz_shop/resources/dimensions.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,14 +12,27 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Home Page",
-          style: appStyle(
-            Dimensions.font15,
-            AppColor.primaryColor,
-            FontWeight.bold,
-          ),
+      body: SizedBox(
+        height: Dimensions.screenHeight,
+        width: Dimensions.screenWidth,
+        child: Stack(
+          children: [
+            Container(
+              height: Dimensions.screenHeight * 0.4,
+              padding: EdgeInsets.fromLTRB(
+                Dimensions.width10,
+                Dimensions.height30,
+                0,
+                0,
+              ),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/bg.png"),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
