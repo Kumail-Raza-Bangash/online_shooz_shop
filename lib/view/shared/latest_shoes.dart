6f75bf49ff5ebcfe854c738dcb903becb8feb4cb
Kumail-Raza-Bangash <kumailraza260@gmail.com>
2024-06-03 14:19:05 +0500

    Updated Code

--- lib/view/shared/latest_shoes.dart
+++ lib/view/shared/latest_shoes.dart
@@ -4,8 +4,8 @@ import 'package:online_shooz_shop/models/sneaker_model.dart';
 import 'package:online_shooz_shop/view/shared/stagger_tile.dart';
 
 // ignore: camel_case_types
-class LatestShoes extends StatelessWidget {
-  const LatestShoes({
+class latestShoes extends StatelessWidget {
+  const latestShoes({
     super.key,
     required Future<List<Sneakers>> male,
   }) : _male = male;
@@ -23,26 +23,30 @@ class LatestShoes extends StatelessWidget {
           return Text("Error ${snapshot.error}");
         } else {
           final male = snapshot.data;
-          return StaggeredGrid.count(
-            crossAxisCount: 2,
+          return MasonryGridView.builder(
+            gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+            ),
+            scrollDirection: Axis.vertical,
             crossAxisSpacing: 20,
             mainAxisSpacing: 16,
-            // itemCount: male!.length,
-            axisDirection: AxisDirection.right,
-            List<Sneakers>: [
-              StaggeredTile.extent(
-                (index % 2 == 0) ? 1 : 1,
-                (index % 4 == 1 || index % 4 == 3)
-                    ? MediaQuery.of(context).size.height * 0.35
-                    : MediaQuery.of(context).size.height * 0.3),
+            itemCount: male!.length,
             itemBuilder: (context, index) {
               final shoe = snapshot.data![index];
               return StaggerTile(
-                imageUrl: shoe.imageUrl[1],
-                name: shoe.name,
-                price: "\$${shoe.price}",
-              );
-            },]  
+                  imageUrl: shoe.imageUrl[1],
+                  name: shoe.name,
+                  price: "\$${shoe.price}");
+            },
+
+            //
+            // staggeredTileBuilder: (index) => StaggeredTile.extent(
+            //   (index % 2 == 0) ? 1 : 1,
+            //   (index % 4 == 1 || index % 4 == 3)
+            //       ? MediaQuery.of(context).size.height * 0.35
+            //       : MediaQuery.of(context).size.height * 0.3,
+            // ),
+            //
           );
         }
       },
